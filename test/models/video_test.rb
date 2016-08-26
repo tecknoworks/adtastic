28efa7a53206926e01fbc0ddc48cba226dc30d3e
razvancostea <razvan.costea@tecknoworks.com>
2016-08-26 16:36:24 +0300

    ds

--- test/models/video_test.rb
+++ test/models/video_test.rb
@@ -4,4 +4,10 @@ class VideoTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test "video test" do
+  	v = Video.new(name: "testvideo")
+  	v.save
+  	result = Video.find(v.id).name
+  	assert_equal(result,"testvideo","Name don't match")
+  end
 end
