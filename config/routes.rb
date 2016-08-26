0b48f5501a6c39391b31a2dfbb421f009c0a17ac
Tudor Stupariu <stupariu.tudorf@gmail.com>
2016-08-26 17:15:57 +0300

    contr

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  get 'users/index'
+
   get 'user/index'
 
-  get 'user_controller/index'
+  get 'user_controller/index', to: "user_controller#index"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
