9c4e40db14712aea748fb0576356409546941e64
Tudor Stupariu <stupariu.tudorf@gmail.com>
2016-08-26 16:28:26 +0300

    Photo test added

--- test/models/photo_test.rb
+++ test/models/photo_test.rb
@@ -4,4 +4,10 @@ class PhotoTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test "Find_The_Name" do
+  	p = Photo.new(name: "testPhoto")
+  	p.save
+  	result = Photo.find(p.id).name
+  	assert_equal(result, "testPhoto", "Names do not match")
+	end
 end
