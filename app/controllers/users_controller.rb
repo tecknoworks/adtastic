80658cb82d390fd490c7a239bf024d9ef487f44c
Tudor Stupariu <stupariu.tudorf@gmail.com>
2016-08-29 10:40:32 +0300

    Controller thingy beginner stuff

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,18 @@
 class UsersController < ApplicationController
   def index
+  	#respond_with User.all
   end
+
+  def create(password,email,user_type)
+  end
+
+  def remove(idx)
+  end
+
+  def update(idx,password,email)
+  end
+
+  def getUserWithID(idx)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  get 'users/index'
+  #get 'users/index'
 
-  get 'user/index'
-
-  get 'user_controller/index', to: "user_controller#index"
+  get 'user/index', to: "users#index"
+  post 'user/new', to: "users#create"
+  delete 'user/remove', to: "users#remove"
+  put 'user/update', to: "users#update"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
