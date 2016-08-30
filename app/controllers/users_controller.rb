a0854c2527b3df4ecd41eb08267f5a36655181f1
Tudor Stupariu <stupariu.tudorf@gmail.com>
2016-08-30 16:17:43 +0300

    Added tests for User Controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,7 @@ class UsersController < ApplicationController
   def create
   	@user = User.new(user_params)
   	@user.save
+    render nothing: true
   end
 
   def new
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -14,32 +14,32 @@ RSpec.describe UsersController, type: :controller do
       current = User.count
       post :create, params: { user: { password: "pwd", email: "t@t.t", user_type: true } }
       expect(response).to have_http_status :ok
-      #expect(User.count).to be_equal(current + 1)
+      expect(User.count).to be_equal(current + 1)
     end
   end
 
-  # describe "DELETE #destroy" do
-    # it "should delete post" do
-      # current = User.count
-      # u = User.new(password: 'pwd', email: 't#t.v', user_type: true)
-      # u.save
-      # idx = u.id
-      # delete "/user.json", params: { id: idx }
-      # expect(response)
-      # User.count.should eq(current)
-    # end
-  # end
+  describe "DELETE #destroy" do
+    it "should delete post" do
+      current = User.count
+      u = User.new(password: 'pwd', email: 't#t.v', user_type: true)
+      u.save
+      idx = u.id
+      delete :destroy, params: { id: idx }
+      expect(response).to have_http_status :no_content
+      expect(User.count).to be_equal(current)
+    end
+  end
 
-  # describe "PUT #update" do
-    # it "should update thingy Mc'Bobber" do
-      # u = User.new(password: 'pwd', email: 't#t.v', user_type: true)
-      # e = u.email
-      # u.save
-      # idx = u.id
-      # put "/users.json", params: { id: idx, user: { email: 'OK' } }
-      # expect(response)
-      # User.find(idx).email.should_not eq(e)
-    # end
-  # end
+  describe "PUT #update" do
+    it "should update thingy Mc'Bobber" do
+      u = User.new(password: 'pwd', email: 't#t.v', user_type: true)
+      e = u.email
+      u.save
+      idx = u.id
+      put :update, params: { id: idx, user: { email: 'OK' } }
+      expect(response).to have_http_status :no_content
+      expect(User.find(idx).email).not_to eq(e)
+    end
+  end
 
 end
