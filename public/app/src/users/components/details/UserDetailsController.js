8e8798085b6678d457928dd0ef8e5b6330519fa1
Tudor Stupariu <stupariu.tudorf@gmail.com>
2016-09-05 10:49:27 +0300

    Adds base index.html page

--- public/app/src/users/components/details/UserDetailsController.js
+++ public/app/src/users/components/details/UserDetailsController.js
@@ -28,20 +28,6 @@ class UserDetailsController  {
       this.$log.debug( clickedItem.name + ' clicked!');
     });
 
-    /**
-     * Bottom Sheet controller for the Avatar Actions
-     */
-    function UserSheetController( $mdBottomSheet ) {
-      this.user = user;
-      this.items = [
-        { name: 'Phone'       , icon: 'phone'       , icon_url: 'assets/svg/phone.svg'},
-        { name: 'Twitter'     , icon: 'twitter'     , icon_url: 'assets/svg/twitter.svg'},
-        { name: 'Google+'     , icon: 'google_plus' , icon_url: 'assets/svg/google_plus.svg'},
-        { name: 'Hangout'     , icon: 'hangouts'    , icon_url: 'assets/svg/hangouts.svg'}
-      ];
-      this.performAction = (action) => {
-        $mdBottomSheet.hide(action);
-      };
     }
   }
 
