e561d905863a14b1313ab136cf84ddf996e57cf7
razvancostea <razvan.costea@tecknoworks.com>
2016-09-07 17:00:10 +0300

    content page mod

--- public/app/routing/controller.js
+++ public/app/routing/controller.js
@@ -46,7 +46,21 @@ app.controller('SignInController', function ($scope, $http, $location) {
        alert("Incorrect login") })
  }
 });
-app.controller('ContentManagerController', function ($scope, $location) {
+app.controller('ContentManagerController', function ($scope, $http, $location) {
+
+  $http.get('/photos.json').then(function (response) {
+    $scope.photos = response.data.photos;
+  }, function (error) {
+    console.log(error);
+  })
+
+  $http.get('/videos.json').then(function (response) {
+    $scope.videos = response.data.videos;
+  }, function (error) {
+    console.log(error);
+  })
+
+
   $scope.content = function(){
     $location.path('/content');
   }
@@ -55,6 +69,63 @@ app.controller('ContentManagerController', function ($scope, $location) {
   }
   $scope.cast = function(){
     $location.path('/cast');
+  }
+
+
+  $scope.deletep = function(pid) {
+
+    $http.delete('/photos/' + pid + '.json', {}).then(function (response) 
+
+    { 
+
+      for (var count = 0; count < $scope.photos.length; count++) 
+
+      {
+
+        if ($scope.photos[count].id == pid) $scope.photos.splice(count,1);
+
+      }
+
+    }, function (error) { console.log("Not requested") })
+
+  }
+
+  $scope.deletev = function(vid) {
+
+    $http.delete('/videos/' + vid + '.json', {}).then(function (response) 
+
+    { 
+
+      for (var count = 0; count < $scope.videos.length; count++) 
+
+      {
+
+        if ($scope.videos[count].id == vid) $scope.videos.splice(count,1);
+
+      }
+
+    }, function (error) { console.log("Not requested") })
+
+  }
+
+  $scope.addphoto = function() {
+
+    $http.post('/photos.json', { photo: { name: $scope.inputName, url: $scope.inputUrl } } ).then(function (response) {
+
+      $scope.photos.push(response.data);
+
+    }, function (error) { console.log("Not requested") });
+
+  }
+
+  $scope.addvideo = function() {
+
+    $http.post('/videos.json', { video: { name: $scope.inputName, url: $scope.inputUrl } } ).then(function (response) {
+
+      $scope.videos.push(response.data);
+
+    }, function (error) { console.log("Not requested") });
+
   }
 
 });
