8cfac563429e6d6a665f09c3f50dcfbf005807e9
Tudor Stupariu <stupariu.tudorf@gmail.com>
2016-09-09 13:19:11 +0300

    Adds protractor E2E tests for tab redirections/routing

--- PROTRACTOR/todo-spec.js
+++ PROTRACTOR/todo-spec.js
@@ -1,11 +1,33 @@
-describe('angularjs homepage todo list', function() {
+describe('ADTASTIC page ', function() {
   it('should have title', function() {
     browser.get('http://localhost:3000/app/index.html');
     expect(browser.getTitle()).toEqual('ADTASTIC');
   });
 
-  it('should add a user', function(){
-  	browser.get('http://localhost:3000/app/index.html#/users');
-  })
+  it('should log in user', function(){
+  	browser.get('http://localhost:3000/app/index.html');
+  	element(by.model('inputEmail')).sendKeys("testmail");
+  	element(by.model('inputPassword')).sendKeys("tesstpwd");
 
+  	element(by.id('1')).click();
+  	expect(browser.getCurrentUrl()).toEqual("http://localhost:3000/app/index.html#/content");
+  });
+
+  it('should redirect to users manager', function(){
+  	browser.get('http://localhost:3000/app/index.html#/content');
+  	element(by.id('1')).click();
+  	expect(browser.getCurrentUrl()).toEqual("http://localhost:3000/app/index.html#/users");
+  });
+
+  it('should redirect to cast menu', function(){
+  	browser.get('http://localhost:3000/app/index.html#/content');
+  	element(by.id('2')).click();
+  	expect(browser.getCurrentUrl()).toEqual("http://localhost:3000/app/index.html#/cast");
+  });
+
+  it('should redirect to users manager', function(){
+  	browser.get('http://localhost:3000/app/index.html#/content');
+  	element(by.id('3')).click();
+  	expect(browser.getCurrentUrl()).toEqual("http://localhost:3000/app/index.html#/device");
+  });
 });
