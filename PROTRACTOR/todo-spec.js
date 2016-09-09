ce230f4088699d888c6cc0a6adac6ea1965edf4b
Tudor Stupariu <stupariu.tudorf@gmail.com>
2016-09-09 12:12:26 +0300

    Added welcome to log in screen

--- PROTRACTOR/todo-spec.js
+++ PROTRACTOR/todo-spec.js
@@ -1,7 +1,11 @@
 describe('angularjs homepage todo list', function() {
-  it('should add a todo', function() {
+  it('should have title', function() {
     browser.get('http://localhost:3000/app/index.html');
-
-    expect(browser.getTitle()).toEqual('ADTASTIC - Log In');
+    expect(browser.getTitle()).toEqual('ADTASTIC');
   });
+
+  it('should add a user', function(){
+  	browser.get('http://localhost:3000/app/index.html#/users');
+  })
+
 });
---
+++ PROTRACTOR/todo-spec.js~
@@ -0,0 +1,7 @@
+describe('angularjs homepage todo list', function() {
+  it('should add a todo', function() {
+    browser.get('http://localhost:3000/app/index.html');
+
+    expect(browser.getTitle()).toEqual('ADTASTIC - Log In');
+  });
+});
