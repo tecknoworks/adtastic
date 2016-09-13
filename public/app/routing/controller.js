9fde2560c461119cdfed39e00f13b35b60e7d1e3
razvancostea <razvan.costea@tecknoworks.com>
2016-09-13 11:56:08 +0300

    menu bar hide and show

--- public/app/routing/controller.js
+++ public/app/routing/controller.js
@@ -35,26 +35,21 @@ app.config(function($mdThemingProvider) {
 });
 
 app.controller('SignInController', function ($scope, $http, $location) {
-  $scope.options = {
-    title: 'Sign In',
-    menuVisible: false
-  }
 
   $scope.test = function() {
    var uname = $scope.inputEmail;
    var password = $scope.inputPassword;
    $http.post('/users/signin', { inputEmail: $scope.inputEmail, inputPassword: $scope.inputPassword }).then(
-     function (response) 
-     { $location.path('/content'); }, 
+     function (response) {
+
+        $location.path('/content');
+      },
      function (error) { 
        alert("Incorrect login") })
  }
 });
 app.controller('ContentManagerController', function ($scope, $http, $location) {
-  $scope.options = {
-    title: 'Sign In',
-    menuVisible: true
-  }
+  $scope.options.menuVisible = true;
 
   $scope.sortOption = ['Name Ascending','Name Descending'];
   $scope.sortOptions = 'Name Ascending';
@@ -113,24 +108,6 @@ app.controller('ContentManagerController', function ($scope, $http, $location) {
       return 0
     })
     }
-  }
-
-
-
-  $scope.content = function(){
-    $location.path('/content');
-  }
-  $scope.users = function(){
-    $location.path('/users');
-  }
-  $scope.cast = function(){
-    $location.path('/cast');
-  }
-  $scope.device = function(){
-    $location.path('/device');
-  }
-  $scope.signOut = function(){
-    $location.path('/')
   }
 
 
@@ -195,238 +172,172 @@ app.controller('ContentManagerController', function ($scope, $http, $location) {
 
 app.controller('UserManagementController', function ($scope, $http, $location) {
 
-//  $scope.options = {
-//   title: 'Sign In',
-//   menuVisible: true
-// }
+  $scope.options.menuVisible = true;
 
-$http.get('/users.json').then(function (response) {
-  $scope.users = response.data.users;
-}, function (error) {
-  console.log(error);
-})
-
-$scope.content = function(){
-  $location.path('/content');
-}
-$scope.gousers = function(){
-  $location.path('/users');
-}
-$scope.cast = function(){
-  $location.path('/cast');
-}
-$scope.device = function(){
-  $location.path('/device');
-}
-$scope.signOut = function(){
-  $location.path('/')
-}
+  $http.get('/users.json').then(function (response) {
+    $scope.users = response.data.users;
+  }, function (error) {
+    console.log(error);
+  })
 
 
-$scope.delete = function(uid) {
 
-  $http.delete('/users/' + uid + '.json', {}).then(function (response) 
-  { 
-    for (var count = 0; count < $scope.users.length; count++) 
-    {
-      if ($scope.users[count].id == uid) $scope.users.splice(count,1);
-    }
-  }, function (error) { console.log("Not requested") })
-}
+  $scope.delete = function(uid) {
 
-$scope.add = function() {
+    $http.delete('/users/' + uid + '.json', {}).then(function (response) 
+    { 
+      for (var count = 0; count < $scope.users.length; count++) 
+      {
+        if ($scope.users[count].id == uid) $scope.users.splice(count,1);
+      }
+    }, function (error) { console.log("Not requested") })
+  }
 
-  $http.post('/users.json', { user: { email: $scope.inputEmail, password: $scope.inputPassword } } ).then(function (response) {
+  $scope.add = function() {
 
-    $scope.users.push(response.data);
+    $http.post('/users.json', { user: { email: $scope.inputEmail, password: $scope.inputPassword } } ).then(function (response) {
 
-  }, function (error) { console.log("Not requested") });
+      $scope.users.push(response.data);
 
-}
+    }, function (error) { console.log("Not requested") });
+
+  }
 
 });
 
 app.controller('CastMenuController', function ($scope, $location, $http) {
 
- $scope.options = {
-  title: 'Sign In',
-  menuVisible: true
-}
+  $scope.options.menuVisible = true;
 
-$scope.sortOption = ['Name Ascending','Name Descending'];
-$scope.sortOptions = 'Name Ascending';
-$scope.media = [];
+  $scope.sortOption = ['Name Ascending','Name Descending'];
+  $scope.sortOptions = 'Name Ascending';
+  $scope.media = [];
 
-$scope.photos = [];
-$scope.videos= [];
+  $scope.photos = [];
+  $scope.videos= [];
 
-$scope.content = function(){
-  $location.path('/content');
-}
-$scope.users = function(){
-  $location.path('/users');
-}
-$scope.cast = function(){
-  $location.path('/cast');
-}
-$scope.device = function(){
-  $location.path('/device');
-}
-$scope.signOut = function(){
-  $location.path('/')
-}
 
-$scope.moveUp = function(xname)
-{
-  var i = 0;
-  idk = 0;
-  for (i=0; i< $scope.media.length; i++)
+  $scope.sortMedia = function() 
   {
-    if ($scope.media[i].name == xname)
+    if ($scope.sortOptions == "Name Ascending")
     {
-      idk = i;
-    }
-  }
-  if ($scope.media[0].name != xname)
-  {
-    aux = $scope.media[idk - 1];
-    $scope.media[idk - 1] = $scope.media[idk];
-    $scope.media[idk] = aux;
-  }
-}
-
-$scope.moveDown = function(xname)
-{
-  var i = 0;
-  idk = 0;
-  for (i=0; i< $scope.media.length; i++)
-  {
-    if ($scope.media[i].name == xname)
-    {
-      idk = i;
-    }
-  }
-  if ($scope.media[$scope.media.length - 1].name != xname)
-  {
-    aux = $scope.media[idk];
-    $scope.media[idk] = $scope.media[idk + 1];
-    $scope.media[idk + 1] = aux;
-  }
-}
-
-$scope.sortMedia = function() 
-{
-  if ($scope.sortOptions == "Name Ascending")
-  {
-    $scope.media.sort(function(a, b){
-      var nameA=a.name.toLowerCase(), nameB=b.name.toLowerCase()
+      $scope.media.sort(function(a, b){
+        var nameA=a.name.toLowerCase(), nameB=b.name.toLowerCase()
       if (nameA < nameB) //sort string ascending
         return -1 
       if (nameA > nameB)
         return 1
       return 0
     })
-  }
-  else
-  {
-    $scope.media.sort(function(a, b){
-      var nameA=a.name.toLowerCase(), nameB=b.name.toLowerCase()
+    }
+    else
+    {
+      $scope.media.sort(function(a, b){
+        var nameA=a.name.toLowerCase(), nameB=b.name.toLowerCase()
       if (nameA < nameB) //sort string ascending
         return 1 
       if (nameA > nameB)
         return -1
       return 0
     })
+    }
+
+
   }
 
-
-}
-
-$http.get('/users.json').then(function (response) {
-  $scope.usersList = response.data.users;
-}, function (error) {
-  console.log(error);
-})
-
-function getMedia()
-{
-  $http.get('/photos.json').then(function (response) {
-    $scope.photos = response.data.photos;
-    getVideos();
+  $http.get('/users.json').then(function (response) {
+    $scope.usersList = response.data.users;
   }, function (error) {
     console.log(error);
   })
-}
 
-function getVideos()
-{
-  $http.get('/videos.json').then(function (response) {
-    $scope.videos = response.data.videos;
-    $scope.media = $scope.photos.concat($scope.videos);
-  }, function (error) {
-    console.log(error);
-  })
-}
+  function getMedia()
+  {
+    $http.get('/photos.json').then(function (response) {
+      $scope.photos = response.data.photos;
+      getVideos();
+    }, function (error) {
+      console.log(error);
+    })
+  }
 
-getMedia();
+  function getVideos()
+  {
+    $http.get('/videos.json').then(function (response) {
+      $scope.videos = response.data.videos;
+      $scope.media = $scope.photos.concat($scope.videos);
+    }, function (error) {
+      console.log(error);
+    })
+  }
+
+  getMedia();
+
+
 });
 
 app.controller('DeviceManagerController', function ($scope, $location, $http) {
 
- $scope.options = {
-  title: 'Sign In',
-  menuVisible: true
-}
+  $scope.options.menuVisible = true;
 
-$http.get('/devices.json').then(function (response) {
-  $scope.devices = response.data.devices;
-}, function (error) {
-  console.log(error);
-})
+  $http.get('/devices.json').then(function (response) {
+    $scope.devices = response.data.devices;
+  }, function (error) {
+    console.log(error);
+  })
 
 
-$scope.content = function(){
-  $location.path('/content');
-}
-$scope.users = function(){
-  $location.path('/users');
-}
-$scope.cast = function(){
-  $location.path('/cast');
-}
-$scope.device = function(){
-  $location.path('/device');
-}
-$scope.signOut = function(){
-  $location.path('/')
-}
 
-$scope.deleted = function(did) {
+  $scope.deleted = function(did) {
 
-  $http.delete('/devices/' + did + '.json', {}).then(function (response) 
+    $http.delete('/devices/' + did + '.json', {}).then(function (response) 
 
-  { 
+    { 
 
-    for (var count = 0; count < $scope.devices.length; count++) 
+      for (var count = 0; count < $scope.devices.length; count++) 
 
-    {
+      {
 
-      if ($scope.devices[count].id == did) $scope.devices.splice(count,1);
+        if ($scope.devices[count].id == did) $scope.devices.splice(count,1);
 
-    }
+      }
 
-  }, function (error) { console.log("Not requested") })
+    }, function (error) { console.log("Not requested") })
 
-}
+  }
 
-$scope.addDevice = function() {
+  $scope.addDevice = function() {
 
-  $http.post('/devices.json', { device: { name: $scope.inputName } } ).then(function (response) {
+    $http.post('/devices.json', { device: { name: $scope.inputName } } ).then(function (response) {
 
-    $scope.devices.push(response.data);
+      $scope.devices.push(response.data);
 
-  }, function (error) { console.log("Not requested") });
+    }, function (error) { console.log("Not requested") });
 
-}
+  }
 
+
+
+});
+
+app.controller('MainController', function ($scope, $rootScope, $http, $location) {
+  $rootScope.options = {
+    menuVisible: false
+  }
+
+  $scope.content = function(){
+    $location.path('/content');
+  }
+  $scope.users = function(){
+    $location.path('/users');
+  }
+  $scope.cast = function(){
+    $location.path('/cast');
+  }
+  $scope.device = function(){
+    $location.path('/device');
+  }
+  $scope.signOut = function(){
+    $location.path('/')
+  }
 });
--- public/app/routing/mainController.js
+++ public/app/routing/mainController.js
@@ -3,7 +3,6 @@ var app = angular.module('mainApp', ['ngRoute', 'ngMaterial']);
 
 app.controller('MainController', function ($scope, $http, $location) {
   $scope.options = {
-    title: 'Sign In',
     menuVisible: true
   }
  });
