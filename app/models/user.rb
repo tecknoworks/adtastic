c88c6d2a4c65642ba9cfe0a1b37687b675f8f7f4
razvancostea <razvan.costea@tecknoworks.com>
2016-09-15 16:05:42 +0300

    Encrypt passwod

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,11 @@
 # model for backend
 class User < ApplicationRecord
-  def self.verify_password(inputEmail, inputPassword)
-    u = User.find_by_email(inputEmail).password
-    return true if u == inputPassword
-    false
-  end
+	has_secure_password
+  # def self.verify_password(inputEmail, inputPassword)
+  #   u = User.find_by_email(inputEmail).password
+  #   return true if u == inputPassword
+  #   false
+  # end
 
   def self.verify_email(inputEmail)
     return true unless User.find_by_email(inputEmail).nil?
