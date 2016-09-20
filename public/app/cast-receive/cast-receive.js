38b5969e434e858ef26a2d4c890d1c73c013bf66
Tudor Stupariu <stupariu.tudorf@gmail.com>
2016-09-20 11:42:04 +0300

    Adds slideshow for fixed interval in cast-receive

--- public/app/cast-receive/cast-receive.js
+++ public/app/cast-receive/cast-receive.js
@@ -1,7 +1,6 @@
 angular.module('mainApp')
-.controller('CastReceiveController', function ($rootScope, $scope, $sce, $routeParams, $cookies) {
+.controller('CastReceiveController', function ($rootScope, $scope, $sce, $routeParams, $cookies, $interval) {
 	$scope.playlist = $cookies.getObject('objSocket');
-	console.log($scope.playlist);
 	getListIndex = function() 
 	{
 		for (i = 0; i < $scope.playlist.item.length; i++)
@@ -13,13 +12,28 @@ angular.module('mainApp')
 		}
 	}
 
-	var idx = getListIndex();
-	for (j = 0; j<$scope.playlist.item[idx].content_url.length; j++)
+
+
+	$scope.idx = getListIndex();
+	for (j = 0; j<$scope.playlist.item[$scope.idx].content_url.length; j++)
 	{
-		console.log($scope.playlist.item[idx].content_url[j]);
-		$scope.playlist.item[idx].content_url[j] = $scope.playlist.item[idx].content_url[j].replace("watch?v=", "v/"); 
-		$scope.playlist.item[idx].content_url[j] = $sce.trustAsResourceUrl($scope.playlist.item[idx].content_url[j]);
+		$scope.playlist.item[$scope.idx].content_url[j] = $scope.playlist.item[$scope.idx].content_url[j].replace("watch?v=", "v/"); 
+		$scope.playlist.item[$scope.idx].content_url[j] = $sce.trustAsResourceUrl($scope.playlist.item[$scope.idx].content_url[j]);
 	}
 
-	$scope.a = $scope.playlist.item[idx].content_url[0];
+	var len = $scope.playlist.item[$scope.idx].content_url.length;
+
+	count = 0;
+	callTimeOut = function(len)
+	{
+		$scope.a = $scope.playlist.item[$scope.idx].content_url[count];
+		count++;
+		if (count == len)
+		{
+			count = 0;
+		}
+	}
+	callTimeOut(len);
+	$interval(function() {callTimeOut(len)}, 3000);
+
 });
